5a46ca7965d11b1421c246985a208c171e82a61c
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-10 12:06:24 +0100

    Implement the App store and routes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,30 @@
 // Entry point for the build script in your package.json
 // import "@hotwired/turbo-rails"
 // import "./controllers"
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import messagesReducer from './redux/messagesSlice';
+import Greeting from './src/components/Greeting';
+
+
+const store = configureStore({
+  reducer: {
+    message: messagesReducer,
+  }
+})
 
 const App = () => {
-  return(
-    <>
-      <h1>Hello World!</h1>
-    </>
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
