d67b219ff1cf43ebe06d8ef5fc70d2f58ed70057
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-03-10 12:07:49 +0100

    Add Greeting component for the app

---
+++ app/javascript/src/components/Greeting.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMessages } from '../../redux/messagesSlice';
+
+const Greeting = () => {
+  const getGreeting = useSelector(state => state.message);
+  console.log(getGreeting);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMessages())
+  }, [dispatch])
+
+  return (
+    <>
+      <h1>Message:</h1>
+      <p>{getGreeting.text}</p>
+    </>
+  )
+}
+
+export default Greeting;
